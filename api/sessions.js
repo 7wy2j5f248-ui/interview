a058a5196afdcd18cb04ab1d1b8d310a34d1e077
7wy2j5f248-ui <7wy2j5f248@privaterelay.appleid.com>
2026-07-07 16:52:56 -0400

    Create sessions.js

---
+++ api/sessions.js
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+
+    const participant = req.query.participant;
+
+    const { data, error } = await supabase
+        .from("interview_messages")
+        .select("Session, Language")
+        .eq("Participant", participant);
+
+    if (error) {
+        return res.status(500).json(error);
+    }
+
+    return res.status(200).json(data);
+
+}
