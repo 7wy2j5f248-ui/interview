f9783a96a11e0b6b3c930456de8c041006a13467
7wy2j5f248-ui <7wy2j5f248@privaterelay.appleid.com>
2026-07-08 22:42:11 -0400

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,31 @@
+const languages = {
+
+    en: {
+
+        welcome: "Welcome to Interview Quest",
+
+        chooseLanguage: "Please choose your language.",
+
+        consent: "Consent Form",
+
+        startInterview: "Start Interview",
+
+        agree: "I agree"
+
+    },
+
+    zh: {
+
+        welcome: "欢迎来到 Interview Quest",
+
+        chooseLanguage: "请选择您的语言。",
+
+        consent: "知情同意书",
+
+        startInterview: "开始访谈",
+
+        agree: "我同意"
+
+    }
+
+};
