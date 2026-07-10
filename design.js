43d98d3abf864263b00c0a09bc2982ce4a98cce6
7wy2j5f248-ui <7wy2j5f248@privaterelay.appleid.com>
2026-07-10 01:31:56 -0400

    Create design.js

---
+++ design.js
@@ -0,0 +1,27 @@
+document.getElementById("saveButton").addEventListener("click", function () {
+
+    const design = {
+
+        researchTitle: document.getElementById("researchTitle").value,
+
+        researchPurpose: document.getElementById("researchPurpose").value,
+
+        interviewTopic: document.getElementById("interviewTopic").value,
+
+        interviewQuestions: document.getElementById("interviewQuestions").value,
+
+        aiRole: document.getElementById("aiRole").value,
+
+        researchGoal: document.getElementById("researchGoal").value,
+
+        endingMessage: document.getElementById("endingMessage").value,
+
+        interviewQuestionCount: document.getElementById("interviewQuestionCount").value,
+
+        maximumInterviewerQuestions: document.getElementById("maximumInterviewerQuestions").value
+
+    };
+
+    console.log(design);
+
+});
