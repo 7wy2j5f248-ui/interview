326973a88fe7df6327bbc654024d8382d42fd2f1
7wy2j5f248-ui <7wy2j5f248@privaterelay.appleid.com>
2026-07-10 02:26:21 -0400

    Update saveDesign.js

--- api/saveDesign.js
+++ api/saveDesign.js
@@ -5,5 +5,15 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY
 );
 export default async function handler(req, res) {
+const design = req.body;
+  const { error } = await supabase
+  .from("research_designs")
+  .insert([design]);
+  if (error) {
+  return res.status(500).json({ error: error.message });
+}
 
+return res.status(200).json({
+  message: "Research design saved."
+});
 }
